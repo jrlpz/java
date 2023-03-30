dee82d0cb793f7008f476f8777aa9e76b14dc2f4
jrlpz <103128629+jrlpz@users.noreply.github.com>
2023-03-30 16:30:56 +0200

    Add files via upload

---
+++ PokeInfo/src/main/java/com/mycompany/json/LeerJSON.java
@@ -0,0 +1,27 @@
+package com.mycompany.json;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import model.Pokemon;
+import java.io.File;
+import java.io.IOException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class LeerJSON {
+
+    public static Pokemon leerPokemon() throws IOException{
+        Pokemon pokemon = null;
+        ObjectMapper objectMapper = new ObjectMapper();
+  objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false); //SI NO EXISTE ATRIBUTO QUE EN JSON SI PASA DE ELLO
+
+        try {
+        
+ pokemon = objectMapper.readValue(new File("C:\\Users\\javir\\Documents\\NetBeansProjects\\Componentes\\pokemon.json"), Pokemon.class);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("error");
+        }
+        return pokemon;
+    } 
+   
+}
