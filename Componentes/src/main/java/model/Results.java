f472f58f8c6a62c622530cdbf9127cb139e50400
jrlpz <103128629+jrlpz@users.noreply.github.com>
2023-03-30 15:57:47 +0200

    Add files via upload

---
+++ Componentes/src/main/java/model/Results.java
@@ -0,0 +1,18 @@
+
+
+
+package model;
+
+import java.util.List;
+
+
+public class Results {
+   
+    public List<Pokemon> results;
+    
+    public List<Pokemon> getResults() {
+        return results;
+    }
+
+ //String pokemonName = results.getResults().get(0).getName();
+}
