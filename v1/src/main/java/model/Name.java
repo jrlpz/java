cece3659d88c31d680eda5d3b96b4085c64ecbb7
jrlpz <103128629+jrlpz@users.noreply.github.com>
2023-03-30 16:15:16 +0200

    Add files via upload

---
+++ v1/src/main/java/model/Name.java
@@ -0,0 +1,18 @@
+
+
+
+package model;
+
+public class Name {
+private String name;
+
+public Name(){
+
+}
+
+    public String getName() {
+        return name;
+    }
+
+
+}
---
+++ v1/src/main/java/model/Results.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.List;
+
+public class Results {
+
+    public List<Pokemon> results;
+
+    public List<Pokemon> getResults() {
+        return results;
+    }
+
+}
